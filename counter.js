8241e2d409012975d60db5ec79cf0ba50ab06b77
Balionelis <blijonasq@gmail.com>
2023-04-02 12:48:36 +0300

    Updated counter.js

--- counter.js
+++ counter.js
@@ -1,13 +1,12 @@
 if (localStorage.getItem('visitorCount') === null) {
-    localStorage.setItem('visitorCount', 0);
-  }
-  
-  if (sessionStorage.getItem('counted') === null) {
-    let count = parseInt(localStorage.getItem('visitorCount')) + 1;
-    localStorage.setItem('visitorCount', count);
-    sessionStorage.setItem('counted', 'true');
-  }
-  
-  let count = parseInt(localStorage.getItem('visitorCount'));
-  document.getElementById('visitor-count').innerHTML = count;
-  
+  localStorage.setItem('visitorCount', 0);
+}
+
+if (localStorage.getItem('visited') === null) {
+  let count = parseInt(localStorage.getItem('visitorCount')) + 1;
+  localStorage.setItem('visitorCount', count);
+  localStorage.setItem('visited', 'true');
+}
+
+let count = parseInt(localStorage.getItem('visitorCount'));
+document.getElementById('visitor-count').innerHTML = count;
